f5ab258904b997396a6df2344b3cd7da6c2cfd4a
toyhammered <daniel@rassiner.com>
2016-09-03 13:10:42 -0700

    still need to add auth

--- app/controllers/check_controller.rb
+++ app/controllers/check_controller.rb
@@ -14,37 +14,68 @@ class CheckController < ApplicationController
       f.puts body
       f.puts tests
     end
+
     puts "*" * 10
     puts "*" * 10
     puts "*" * 10
     puts "*" * 10
+
+    # Need to check if the ruby code has syntax errors.
+      # * If it does, we skip rspec and return errors
+      # * If it does not, then we can run rspec tests
 
     results = `docker run --rm -v /tmp/#{unique_id}:/opt/code ruby:2.3rspec rspec -f json /opt/code/code.rb`
     results = JSON.parse(results)
-    p results['examples'][0]['description']
-    raise
-    results = results.split(/(?=[0-9]+\))/)
-    results.shift # removes the FFFFF and Failures part
-    p results.first
+
     filtered_answer = extractor(results)
 
     render json: filtered_answer
+    # render json: results
+
+  rescue => e
+    p "Error caught"
+    render json: { errors: "There was a syntax error in your code." }, status: 422
   end
+
 
   def extractor(results)
     answer = []
-    results.each do |result|
+    results['examples'].each do |result|
+      next if result['status'] == 'passed'
+
+      message = result['exception']['message'].split("\n").delete_if(&:blank?).map(&:strip)
       answer.push({
-        expected: result.match(regex("expected"))[0],
-        got: result.match(regex("got"))[0],
+        full_description: result['full_description'],
+        status: result['status'],
+        exception: {
+          class: result['exception']['class'],
+          message: {
+            expected: message[0],
+            got: message[1],
+            compared: message[2]
+          }
+        },
       })
     end
-
+    answer.unshift({
+      summary_line: results['summary_line']
+    })
     answer
   end
-
-  def regex(word)
-    /(?<=#{word}:).+(?=(\\n)+)?/
-  end
+  # def extractor(results)
+  #   answer = []
+  #   results.each do |result|
+  #     answer.push({
+  #       expected: result.match(regex("expected"))[0],
+  #       got: result.match(regex("got"))[0],
+  #     })
+  #   end
+  #
+  #   answer
+  # end
+  #
+  # def regex(word)
+  #   /(?<=#{word}:).+(?=(\\n)+)?/
+  # end
 
 end
