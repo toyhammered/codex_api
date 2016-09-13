f316a6a250f0e5c3ca3582f480a6582d135252f3
toyhammered <daniel@rassiner.com>
2016-09-12 20:02:25 -0700

    puma preload app false

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-set :puma_preload_app, true
+set :puma_preload_app, false
 set :puma_worker_timeout, nil
 set :puma_init_active_record, false  # Change to false when not using ActiveRecord
 
